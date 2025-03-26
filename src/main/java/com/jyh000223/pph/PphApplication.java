68cd5a480ba601a86db4b0399a4df6a264d5dfb7
unknown <https://github.com/YH0223/Titanic_JYH>
2025-03-26 20:08:23 +0900

    first commit

---
+++ src/main/java/com/jyh000223/pph/PphApplication.java
@@ -0,0 +1,13 @@
+package com.jyh000223.pph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PphApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PphApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jyh000223/pph/PphApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jyh000223.pph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PphApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
